fadd9cfe31be66cf95d6d33129bc84443ee176f2
sandeep <spathak1698@gmail.com>
2026-05-25 15:13:46 +0530

    reference and slice learning in rust

--- rust/src/bin/ownership.rs
+++ rust/src/bin/ownership.rs
@@ -24,8 +24,35 @@ fn main() {
 
     println!("{s1}");
 
+    // we can not have two mutable referece a time 
+
+    {
+        let r1 = &mut s1;
+    } // r1 goes out of scope here, so we can make a new reference with no problems.
+
+    let r2 = &mut s1;
+
+    // we can not have mutable reference while we have immutable reference
+    let r1 = &s1; // no problem
+    let r2 = &s1; // no problem
+    println!("{r1} and {r2}");
+    // Variables r1 and r2 will not be used after this point.
+
+    let r3 = &mut s1; // no problem
+    println!("{r3}");
+
+    let reference_to_nothing = dangle();
+    println!("{}", first_word(&reference_to_nothing));
+    let hello = &reference_to_nothing[0..5];
+    println!("{hello}");
+
 }
 
+fn dangle() -> String {
+    let s = String::from("Hello world");
+
+    s
+}
 fn change(s: &mut String) {
     s.push_str(", world 2");
 }
@@ -40,4 +67,16 @@ fn takes_ownership(some_string: String) {
 
 fn makes_copy(some_integer: i32) {
     println!("{some_integer}");
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
 }
