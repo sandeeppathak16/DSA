e74977fcc18f2149c20cb43e03d16b219d23db88
sandeep <spathak1698@gmail.com>
2026-07-20 21:49:54 +0530

    completed mini grep project

--- Rust/minigrep/src/main.rs
+++ Rust/minigrep/src/main.rs
@@ -1,16 +1,58 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
+use minigrep::{search, search_case_insensitive};
+
+
+struct Config {
+    file_path: String,
+    query: String,
+    ignore_case: bool,
+}
+
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config { query, file_path, ignore_case })
+    }
+}
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Search for query {query}");
-    println!("In file {file_path}");
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-    println!("with text:\n{contents:?}");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
 }
